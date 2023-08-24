900e58d53f1e483cd0151a589141c48a863dd3ae
kumarvivekpandey <35356580+kumarvivekpandey@users.noreply.github.com>
2023-08-24 05:51:11 +0000

    DONE

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,85 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import MovieBox from './MovieBox';
-import Navbar from './Navbar';
+import { Navbar, Container, Nav, Form, FormControl, Button } from 'react-bootstrap';
+
+const API_URL = "https://api.themoviedb.org/3/movie/popular?api_key=5ef1bf21a758b6a54008253a54e3ee4d";
+const API_SEARCH = "https://api.themoviedb.org/3/search/movie?api_key=5ef1bf21a758b6a54008253a54e3ee4d&query";
+
 
 export default function App() {
-  const API_URL="https://api.themoviedb.org/3/movie/popular?api_key=5ef1bf21a758b6a54008253a54e3ee4d"
-  
-  const[movies,setMovies]=useState([]);
-  useEffect(()=>{
+
+  const [movies, setMovies] = useState([]);
+  const [query, setQuery] = useState('');
+  useEffect(() => {
     fetch(API_URL)
-    .then((res)=>res.json()).then(data=>{
-      console.log(data)
+      .then((res) => res.json())
+      .then(data => {
+        console.log(data);
+        setMovies(data.results);
+      })
+  }, [])
+
+
+  const searchMovie = async (e) => {
+    e.preventDefault();
+    console.log("Searching");
+    try {
+      const url=`https://api.themoviedb.org/3/search/movie?api_key=5ef1bf21a758b6a54008253a54e3ee4d&query=${query}`;
+      const res = await fetch(url);
+      const data = await res.json();
+      console.log(data);
       setMovies(data.results);
-    })
-  },[])
+    }
+    catch (e) {
+      console.log(e)
+    }
+  }
+  const changeHandler = (e) => {
+    setQuery(e.target.value);
+  }
   return (
-    <div className="conatiner pt-2">
-      <Navbar />
-      <div className="grid">
-      {movies.map((movieReq)=><MovieBox key={movieReq.id} {...movieReq} />)}
-      </div>
+    <>
+      <Navbar bg="dark" expand="lg" variant="dark">
+        <Container fluid>
+          <Navbar.Brand href="/home">MovieDb App</Navbar.Brand>
+          <Navbar.Brand href="/home">Trending</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll"></Navbar.Toggle>
+
+          <Navbar.Collapse id="nabarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-3"
+              style={{ maxHeight: '100px' }}
+              navbarScroll></Nav>
+
+            <Form className="d-flex" onSubmit={searchMovie} autoComplete="off">
+              <FormControl
+                type="search"
+                placeholder="Movie Search"
+                className="me-2"
+                aria-label="search"
+                name="query"
+                value={query} onChange={changeHandler}
+              ></FormControl>
+              <Button variant="secondary" type="submit">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+     
     
-    </div>
+        <div className="conatiner">
+ {movies.length > 0 ?(
+          <div className="grid">
+            {movies.map((movieReq) => <MovieBox key={movieReq.id} {...movieReq} />)}
+          </div>
+         ):(
+        <h2>Sorry !! No Movies Found</h2>
+      )}
+        </div>
+       
+    </>
+
   )
 }
